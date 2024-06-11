8cbf38f70b8aa7902e3b1fed2e92ecb7e2edabf9
Dawn Trail <dawn.trail@thetin.net>
2024-06-11 17:47:04 +0100

    creating event details component

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import EventService from '@/services/EventService'
+
+const event = ref(null)
+const id = ref(123)
+
+onMounted(() => {
+  EventService.getEvent(id)
+    .then((res) => {
+      event.value = res.data
+    }).catch((err) => {
+      console.log(err)
+    })
+})
+
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+  <div class="event-details">
+    <p>{{ event.title }}</p>
+    <p>{{ event.time }}</p>
+  </div>
+</template>
+
+<style scoped></style>
