8e649aeaa06940adefec8dd53ca9cd4d53c4b1c4
Dawn Trail <dawn.trail@thetin.net>
2024-06-17 18:32:14 +0100

    adding page numbers the page numbers

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -7,14 +7,16 @@ const events = ref(null)
 const props = defineProps(['page'])
 const page = computed(() => props.page)
 const totalEvents = ref(0)
-const eventsPerPage = 2
-
+const eventsPerPage = 1
 
 const hasNextPage = computed(() => {
-  const totalPages = Math.ceil(totalEvents.value / 2)
+  const totalPages = Math.ceil(totalEvents.value / eventsPerPage)
   return page.value < totalPages
 })
 
+const pagination = computed(() => {
+  return Math.ceil(totalEvents.value / eventsPerPage)
+})
 
 onMounted(() => { // only called on initial load so wrap api call in watchEffect
   watchEffect(() => { // when reactive components change, api call happens again
@@ -34,14 +36,21 @@ onMounted(() => { // only called on initial load so wrap api call in watchEffect
 
 <template>
   <h1>Events for Good</h1>
+  Total Pages: {{ pagination }}
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
 
-    <div id="pagination"> <router-link id="page-prev" :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev"
-        v-if="page != 1">
-        <span>&#8592</span> Previous</router-link>
+    <div id="pagination">
+      <router-link id="page-prev" :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">
+        <p v-if="page != 1">Previous</p>
+      </router-link>
+
+      <router-link id="page-numbers" v-for="(page, i) in pagination" :key="i"
+        :to="{ name: 'event-list', query: { page: page } }" rel="page 1">{{ page }}</router-link>
+
       <router-link id="page-next" :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next"
-        v-if="hasNextPage">Next <span>&#8594</span> </router-link>
+        v-if="hasNextPage">Next
+      </router-link>
     </div>
   </div>
 
@@ -56,5 +65,27 @@ onMounted(() => { // only called on initial load so wrap api call in watchEffect
 #pagination {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+}
+
+#page-prev {
+  content: "&#8592";
+  display: inline-block;
+  margin-right: 10px;
+  min-width: 62px;
+}
+
+
+#page-next {
+  content: "&#8594";
+  display: inline-block;
+  margin-left: 20px
+}
+
+#page-numbers {
+  display: inline-block;
+  border: 1px solid black;
+  padding: 5px 10px;
+  margin-inline: 2px;
 }
 </style>
