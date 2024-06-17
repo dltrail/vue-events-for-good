31bd6d1fcdf0008b859df35cad8d4e37a3b13316
Dawn Trail <dawn.trail@thetin.net>
2024-06-17 17:01:51 +0100

    add paginations with router links

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import EventService from '@/services/EventService'
 
 const events = ref(null)
+const props = defineProps(['page'])
+const page = computed(() => props.page)
 
 onMounted(() => {
-  EventService.getEvents()
+  EventService.getEvents(1, page.value)
     .then((res) => {
       events.value = res.data
     }).catch((err) => {
@@ -20,7 +22,12 @@ onMounted(() => {
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev" v-if="page != 1">Prev</router-link>
+    <router-link :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">Next</router-link>
+
   </div>
+
 </template>
 <style scoped>
 .events {
