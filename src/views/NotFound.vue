719b64f64fe41b3d7f1ee1e5e59285dbaddc978e
Dawn Trail <dawn.trail@thetin.net>
2024-06-20 16:15:16 +0100

    create not found view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div id="not-found">
+    <h1>Oooops</h1>
+    <p>This is akward - we can't find the page you're looking for</p>
+    <p>Get me out of here</p>
+
+  </div>
+</template>
+
+<style scoped></style>
