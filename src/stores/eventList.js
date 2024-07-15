f7b2f852c7a913c845ab3d2740cd9b29b90493e0
Dawn Trail <dawn.trail@thetin.net>
2024-07-15 11:14:38 +0100

    add store state a simple actions

--- src/stores/eventList.js
+++ src/stores/eventList.js
@@ -3,8 +3,18 @@ import { defineStore } from 'pinia'
 export const useEventStore = defineStore('eventList', {
   //state
   state: () => ({
-    eventList: []
-  })
+    eventList: [],
+    id: 1001
+  }),
   //getters
-  //actions
+  actions: {
+    addEvent(event) {
+      this.toList.push({ event, id: this.id++, boosted: false })
+    },
+    deleteEvent(eventId) {
+      this.toList = this.toList.filter((object) => {
+        return object.id !== eventId
+      })
+    }
+  }
 })
