6e5b6c168fc0f4680db01ac6c9f89d68c70f0a61
Dawn Trail <dawn.trail@thetin.net>
2024-07-15 10:43:42 +0100

    force scroll to top when page is reloaded

--- src/router/index.js
+++ src/router/index.js
@@ -100,7 +100,14 @@ const router = createRouter({
         }
       }
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 router.beforeEach((to, from) => {
