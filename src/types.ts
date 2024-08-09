ebb7f0c99e2eff3a21deea93c842da1775fc83b8
Dawn Trail <dawn.trail@thetin.net>
2024-08-09 12:21:00 +0100

    create types declaration file

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+// Application types
+
+export type EventItem = {
+  id: number
+  category: string
+  title: string
+  description: string
+  location: string
+  date: string
+  time: string
+  organizer: string
+}
