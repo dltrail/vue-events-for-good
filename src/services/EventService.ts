3c5b79eea25436ad406fc3f5ede27ceab2611944
Dawn <dtrail76@gmail.com>
2024-08-15 15:24:02 +0100

    update project name and endpoint

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/dltrail/real-word-vue-3',
+  baseURL: 'https://my-json-server.typicode.com/dltrail/vue-events-for-good',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
